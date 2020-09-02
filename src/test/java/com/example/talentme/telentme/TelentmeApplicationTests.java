f21334dacb9c0d92692d00039d09e01d7ae82e15
WEICHUANJUN <weichuanjun0528@gmail.com>
2020-09-02 18:28:47 +0900

    Initial commit

---
+++ src/test/java/com/example/talentme/telentme/TelentmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talentme.telentme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelentmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
