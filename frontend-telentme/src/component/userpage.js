ac0f2a65027ca99d7885027c40d7330754f8df26
WEICHUANJUN <weichuanjun0528@gmail.com>
2020-09-04 10:48:28 +0900

    first commit

---
+++ frontend-telentme/src/component/userpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class  user extends Component {
+
+    state = {  }
+    render() { 
+        return ( 
+            const 
+         );
+    }
+}
+ 
+export default ;
